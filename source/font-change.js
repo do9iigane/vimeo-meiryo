9a90e376f9e8746ffdbd95482a26ffecfac227e0
毒眼鏡 <>
2012-11-16 12:49:44 +0900

    メイン・ソース追加

---
+++ source/font-change.js
@@ -0,0 +1,12 @@
+//font-family change
+font = '"meiryo"';
+//$('.home').css('font-family',font);
+//$('.inputpassword').css('font-family',font);
+//$('.inputtext').css('font-family',font);
+//$('.uiButtonText').css('font-family',font);
+//$('.uiButton').css('font-family',font);
+
+//$('textarea').css('font-family',font);
+$('body').css('font-family',font);
+//$('input').css('font-family',font);
+//alert('loaded');
